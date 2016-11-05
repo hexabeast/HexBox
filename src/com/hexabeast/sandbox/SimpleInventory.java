f6a7951be8bbda19e2b06809f168249b714c37b5
Hexabeast <hexabeastdev@gmail.com>
2016-11-05 22:30:10 +0100

    Removal of useless things

--- src/com/hexabeast/sandbox/SimpleInventory.java
+++
@@ -1,8 +0,0 @@
-package com.hexabeast.sandbox;
-
-public class SimpleInventory {
-	public SimpleInvItem[][] SimpleinvItemsArray = new SimpleInvItem[10][13];
-	public SimpleInventory() {
-	}
-
-}
--- src/com/hexabeast/sandbox/mobs/MobSave.java
+++
@@ -1,9 +0,0 @@
-package com.hexabeast.sandbox.mobs;
-
-public class MobSave {
-	public int[] type;
-	public float[] x;
-	public float[] y;
-	
-	public MobSave() {}
-}
